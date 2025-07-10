a0f8aa04ba6d36c360acab2d592a16b305a2367c
huodoushigemi <2629878269@qq.com>
2025-07-10 18:30:42 +0800

    Update Table.tsx

--- src/extensions/Table.tsx
+++ src/extensions/Table.tsx
@@ -3,46 +3,57 @@ import { createEffect, createMemo, useTransition } from 'solid-js'
 import { xor } from 'es-toolkit'
 import { useEditorTransaction } from '../Editor'
 import { log } from '../utils'
+import { Floating } from '../components/Popover'
+import { Menu } from '../components/Menu'
 
-export const menus = (editor: Editor) => {
-  // const mutiCell = 
-  // editor.view.state.selection.
-  // const
-  // 
-  // tableCell
-  // tableHeader
+export const menus = []
+
+const menus2 = (editor: Editor) => {
   editor.state.doc.nodeAt
   const name = ['tableCell', 'tableHeader']
-  // const cell = useEditorTransaction(editor, editor => [
-  //   findParentNodeClosestToPos(editor.state.selection.$from, e => name.includes(e.type.name))?.node,
-  //   findParentNodeClosestToPos(editor.state.selection.$to, e => name.includes(e.type.name))?.node,
-  // ], {
-  //   equals: (a, b) => a[0] == b[0] && a[1] == b[1]
-  // })
-
-  // editor.state.selection.ranges.filter(e => e.$from.node().type.name == 'tableCell')
   const cell = useEditorTransaction(editor, editor => (
     editor.state.selection.ranges.map(e => findParentNodeClosestToPos(e.$from, e => name.includes(e.type.name))?.node).filter(e => e)
   ), {
     equals: (a, b) => !xor(a || [], b || []).length
   })
 
-  // createEffect(() => {
-  //   log(cell())
-  // })
-
-  // editor.state.selection.ranges
-
   return createMemo(() => {
     if (!cell().length) return
     const ret = [] as any[]
-    ret.push({ icon: <IMySplitCell />, class: 'text-nowrap', children: [
-      { label: '拆分行' },
-      { label: '拆分列' },
+
+    if (cell().length == 1) {
+      if (cell()[0]!.attrs.colspan > 1 || cell()[0]?.attrs.rowspan > 1) {
+        ret.push({ icon: <IMySplitCell />, cb: () => editor.chain().splitCell().focus().run() })
+      }
+    }
+    else if (cell().length > 1) {
+      ret.push({ icon: <IMyMergeCell />, cb: () => editor.chain().mergeCells().focus().run() })
+    }
+
+    ret.push({ icon: <ILucideTrash2 />, class: 'text-nowrap', children: [
+      { label: '删除行', cb: () => editor.chain().deleteRow().focus().run() },
+      { label: '删除列', cb: () => editor.chain().deleteColumn().focus().run() },
     ] })
-    // muti
-    // if (start != end) ret.push({ icon: <IMyMergeCell /> })
-    if (cell().length > 1) ret.push({ icon: <IMyMergeCell /> })
+
     return ret
+  })
+}
+
+export const mounted = (editor: Editor) => {
+  const table = useEditorTransaction(editor, editor => findParentNodeClosestToPos(editor.state.selection.$from, node => node.type.name == 'table'))
+  // editor.
+
+  const toRect = (e) => DOMRect.fromRect({ x: e.left, y: e.top, width: e.right - e.left, height: e.bottom - e.top })
+  const rect = useEditorTransaction(editor, editor => table() && toRect(editor.view.coordsAtPos(table()!.pos)))
+
+  createEffect(() => {
+    // if (!table()) return
+    if (!rect()) return
+    <Floating
+      reference={{ getBoundingClientRect: () => rect()! }}
+      floating={<Menu items={menus2(editor)} />}
+      placement={'top-end'}
+      portal={document.body}
+    />
   })
 }
